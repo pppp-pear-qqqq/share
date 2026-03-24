9a2d75f085c42a46e00255ca7b83d26d3edf4617
pppp-pear-qqqq <120021668+pppp-pear-qqqq@users.noreply.github.com>
2026-03-24 23:31:35 +0900

    Update script.js

--- nav/register/script.js
+++ nav/register/script.js
@@ -1,7 +1,7 @@
 import { } from '../navigator.min.js';
 
 const nav = new NavigatorToaster({
-	api: 'TODO',
+	api: 'https://script.google.com/macros/s/AKfycbznftumO8l0a4w3xj1xAb0jN9cIQGzfN2JmirTnoaDEostWukJ7EP5t0IUAF10h4Zbv/exec',
 	is_subdir: false,
 	is_query_page: false,
 });
