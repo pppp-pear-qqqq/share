23d170ef2d4bfcefb5e31b9412f835e34d6745bb
itt-n <itt_nakashima@worksid.co.jp>
2026-03-30 09:41:06 +0900

    nav/index

--- nav/register/script.js
+++ nav/register/script.js
@@ -1,9 +1,31 @@
-import { } from '../navigator.min.js';
+// import { } from '../navigator.min.js';
 
-const nav = new NavigatorToaster({
-	api: 'https://script.google.com/macros/s/AKfycbznftumO8l0a4w3xj1xAb0jN9cIQGzfN2JmirTnoaDEostWukJ7EP5t0IUAF10h4Zbv/exec',
-	is_subdir: false,
-	is_query_page: false,
+// const nav = new NavigatorToaster({
+// 	api: 'https://script.google.com/macros/s/AKfycbznftumO8l0a4w3xj1xAb0jN9cIQGzfN2JmirTnoaDEostWukJ7EP5t0IUAF10h4Zbv/exec',
+// 	is_subdir: false,
+// 	is_query_page: false,
+// });
+
+// nav.start();
+
+document.getElementById('navigator-form').addEventListener('submit', function (e) {
+	e.preventDefault();
+	const eno = this.children.namedItem('eno').value;
+	const name = this.children.namedItem('name').value;
+	/** @type {HTMLTableElement} */
+	const words = this.querySelector('.words');
+	let word_list = {};
+	for (let i = 0; i < words.tBodies[0].rows.length; ++i) {
+		const row = words.tBodies[0].rows[i];
+		word_list[row.children.namedItem('path').value + '?' + row.children.namedItem('trigger').value] = row.children.namedItem('word').value;
+	}
+	fetch(this.action, {
+		method: this.method.toUpperCase(),
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({ key: eno + '/' + name, words: word_list }),
+	}).then((res) => {
+		if (!res.ok) throw new Error(res.statusText);
+	}).catch((e) => {
+		console.error(e);
+	});
 });
-
-nav.start();
