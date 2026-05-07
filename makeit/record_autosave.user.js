5abd17b4311b17b9e4b1a2e09890c0eaf978c274
pppp-pear-qqqq <120021668+pppp-pear-qqqq@users.noreply.github.com>
2026-05-07 17:26:34 +0900

    Update record_autosave.user.js

--- makeit/record_autosave.user.js
+++ makeit/record_autosave.user.js
@@ -4,7 +4,7 @@
 // @version      0.1.0
 // @updateURL    https://pppp-pear-qqqq.github.io/share/makeit/record_autosave.user.js
 // @downloadURL  https://pppp-pear-qqqq.github.io/share/makeit/record_autosave.user.js
-// @description  ショップから目当ての商品を探すのを補助します。
+// @description  ページ遷移時にタイトルと本文を自動保存して、次の表示時に読み込みます。
 // @author       なしなし
 // @match        https://sapphiredevil.sakura.ne.jp/makeit/btest/public_html/record_edit.php
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=sakura.ne.jp
@@ -13,13 +13,13 @@
 
 (function () {
 	'use strict';
-	const editor = document.querySelector('#editor-area');
+	const title = document.querySelector('[name="title"]');
+	const body = document.querySelector('#editor-area');
+	title.value = localStorage.getItem('record_autosave/title') ?? '';
+	body.value = localStorage.getItem('record_autosave/body') ?? '';
+
 	window.addEventListener('unload', () => {
-		const content = editor.value;
-		localStorage.setItem('record_autosave', content);
+		localStorage.setItem('record_autosave/title', title.value);
+		localStorage.setItem('record_autosave/body', body.value);
 	});
-	const content = localStorage.getItem('record_autosave');
-	if (content) {
-		editor.value = content;
-	}
 })();
